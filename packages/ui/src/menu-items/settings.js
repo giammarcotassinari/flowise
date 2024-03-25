c752395c512f6aaa92ea3ce5132ad64df25ff61f
giammarcotassinari <giammarco.t@jumpgroup.it>
2024-03-25 11:03:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ packages/ui/src/menu-items/settings.js
@@ -0,0 +1,66 @@
+// assets
+import { IconTrash, IconFileUpload, IconFileExport, IconCopy, IconMessage, IconAdjustmentsHorizontal } from '@tabler/icons'
+
+// constant
+const icons = {
+    IconTrash,
+    IconFileUpload,
+    IconFileExport,
+    IconCopy,
+    IconMessage,
+    IconAdjustmentsHorizontal
+}
+
+// ==============================|| SETTINGS MENU ITEMS ||============================== //
+
+const settings = {
+    id: 'settings',
+    title: '',
+    type: 'group',
+    children: [
+        {
+            id: 'viewMessages',
+            title: 'View Messages',
+            type: 'item',
+            url: '',
+            icon: icons.IconMessage
+        },
+        {
+            id: 'chatflowConfiguration',
+            title: 'Configuration',
+            type: 'item',
+            url: '',
+            icon: icons.IconAdjustmentsHorizontal
+        },
+        {
+            id: 'duplicateChatflow',
+            title: 'Duplicate Chatflow',
+            type: 'item',
+            url: '',
+            icon: icons.IconCopy
+        },
+        {
+            id: 'loadChatflow',
+            title: 'Load Chatflow',
+            type: 'item',
+            url: '',
+            icon: icons.IconFileUpload
+        },
+        {
+            id: 'exportChatflow',
+            title: 'Export Chatflow',
+            type: 'item',
+            url: '',
+            icon: icons.IconFileExport
+        },
+        {
+            id: 'deleteChatflow',
+            title: 'Delete Chatflow',
+            type: 'item',
+            url: '',
+            icon: icons.IconTrash
+        }
+    ]
+}
+
+export default settings
